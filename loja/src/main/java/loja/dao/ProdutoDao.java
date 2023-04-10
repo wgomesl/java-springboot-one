c541643be741d9b3d47a2fa70928353a47c467d5
wesley <wesley.gomes222@hotmail.com>
2023-04-10 16:06:57 -0300

    consultando entidades

--- loja/src/main/java/loja/dao/ProdutoDao.java
+++ loja/src/main/java/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import loja.modelo.Produto;
@@ -14,5 +16,23 @@ public class ProdutoDao {
 	
 	public void Cadastrar(Produto produto) {
 		this.em.persist(produto);
+	}
+	
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+	
+	public void remover(Produto produto) {
+		produto = em.merge(produto);
+		this.em.remove(produto);
+	}
+	
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos(){
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 }
