f8e4f4142c53d70094f2d6c3a82c76a88931af8f
wesley <wesley.gomes222@hotmail.com>
2023-04-10 12:18:06 -0300

    mapeamento de atributos

---
+++ loja/src/main/java/loja/modelo/Categoria.java
@@ -0,0 +1,8 @@
+package loja.modelo;
+
+public enum Categoria {
+	
+	CELULARES,
+	INFORMATICA,
+	LIVROS;
+}
--- loja/src/main/java/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/loja/testes/CadastroDeProduto.java
@@ -7,16 +7,14 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import loja.dao.ProdutoDao;
+import loja.modelo.Categoria;
 import loja.modelo.Produto;
 import loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
 		
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("Muito legal");
-		celular.setPreco(new BigDecimal("800"));
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
 		
 		EntityManager em = JPAUtil.getEntityManager();
 		
