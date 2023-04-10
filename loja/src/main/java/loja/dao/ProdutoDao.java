e4b68b9443c4116ea174a45bb16c21b4af8d2b9d
wesley <wesley.gomes222@hotmail.com>
2023-04-10 16:40:38 -0300

    consulta com filtros

--- loja/src/main/java/loja/dao/ProdutoDao.java
+++ loja/src/main/java/loja/dao/ProdutoDao.java
@@ -1,5 +1,6 @@
 package loja.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -34,5 +35,26 @@ public class ProdutoDao {
 	public List<Produto> buscarTodos(){
 		String jpql = "SELECT p FROM Produto p";
 		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+	
+	public List<Produto> buscarPorNome(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public List<Produto> buscarPorNomeDaCategoria(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public BigDecimal buscarPrecoDoProdutoComNome(String nome){
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, BigDecimal.class)
+				.setParameter("nome", nome)
+				.getSingleResult();
 	}
 }
