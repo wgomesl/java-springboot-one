6ac8643804972af5998758f0f9a385cb635dc37c
wesley <wesley.gomes222@hotmail.com>
2023-04-10 21:38:34 -0300

    relacionamentos bidirecionais

--- loja/src/main/java/loja/modelo/Pedido.java
+++ loja/src/main/java/loja/modelo/Pedido.java
@@ -2,6 +2,7 @@ package loja.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -25,14 +26,19 @@ public class Pedido {
 	@ManyToOne
 	private Cliente cliente;
 	
-	@OneToMany
-	private List<ItemPedido> itens;
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
 	
 	public Pedido() {
 	}
 	
 	public Pedido(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public void adicionarItem(ItemPedido item) {
+		item.setPedido(this);
+		this.itens.add(item);
 	}
 
 	public Long getId() {
