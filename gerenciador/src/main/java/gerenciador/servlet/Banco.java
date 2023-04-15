b8215f6d2db37f57b2f591e7ced5684d8c0b5607
wesley <wesley.gomes222@hotmail.com>
2023-04-15 14:59:08 -0300

    listando empresas com servlet

--- gerenciador/src/main/java/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/gerenciador/servlet/Banco.java
@@ -6,6 +6,21 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Resilia");
+		Empresa empresa3 = new Empresa();
+		empresa3.setNome("Capgemini");
+		Empresa empresa4 = new Empresa();
+		empresa4.setNome("Dio");
+		lista.add(empresa);
+		lista.add(empresa2);
+		lista.add(empresa3);
+		lista.add(empresa4);
+	}
 
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
