3d32dec2cafa8a46edec0aedb7656ac082de09c4
wesley <wesley.gomes222@hotmail.com>
2023-04-15 18:51:44 -0300

    listando empresas com jsp

--- gerenciador/src/main/java/gerenciador/servlet/ListaEmpresasServlet.java
+++ gerenciador/src/main/java/gerenciador/servlet/ListaEmpresasServlet.java
@@ -1,9 +1,9 @@
 package gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,15 +19,11 @@ public class ListaEmpresasServlet extends HttpServlet {
 		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
 		
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}	
-		out.println("</ul>");
-		out.println("</body></html>");
+		request.setAttribute("empresas", lista);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
 	}
 
 }
