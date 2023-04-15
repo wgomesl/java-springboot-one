6199e9c68c36d311fada04cf88d382b44f68805d
wesley <wesley.gomes222@hotmail.com>
2023-04-15 18:27:21 -0300

    conectando arquivo servlet com jsp

--- gerenciador/src/main/java/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,8 +1,8 @@
 package gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,8 +29,10 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+		//chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 
 }
