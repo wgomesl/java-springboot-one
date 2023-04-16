1e949cffa30861325593ed9486df2ae35aedceb4
wesley <wesley.gomes222@hotmail.com>
2023-04-16 10:48:01 -0300

    usando a taglib fmt

--- gerenciador/src/main/java/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,13 +21,26 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	//Agora este método só irá atender requisiçoes post (doPost)
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		String dataEmpresa = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(dataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
 		//Criando uma empresa
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
--- gerenciador/src/main/webapp/listaEmpresas.jsp
+++ gerenciador/src/main/webapp/listaEmpresas.jsp
@@ -2,6 +2,8 @@
     pageEncoding="ISO-8859-1"%>
 <%@ page import="java.util.List, gerenciador.servlet.Empresa" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +15,7 @@
 	
 	<ul>
 		<c:forEach items="${empresas}" var="empresa">
-		<li>${empresa.nome}</li>
+		<li>${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/></li>
 		</c:forEach>
 	</ul>
 </body>
