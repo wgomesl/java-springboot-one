6a6f861e494fd0137050250453697a7638bd8bfb
wesley <wesley.gomes222@hotmail.com>
2023-04-16 20:41:57 -0300

    removendo empresas

--- gerenciador/src/main/java/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/gerenciador/servlet/Banco.java
@@ -1,20 +1,29 @@
 package gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
+		
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Resilia");
+		
 		Empresa empresa3 = new Empresa();
+		empresa3.setId(chaveSequencial++);
 		empresa3.setNome("Capgemini");
+		
 		Empresa empresa4 = new Empresa();
+		empresa4.setId(chaveSequencial++);
 		empresa4.setNome("Dio");
 		lista.add(empresa);
 		lista.add(empresa2);
@@ -23,11 +32,25 @@ public class Banco {
 	}
 
 	public void adiciona(Empresa empresa) {
+		empresa.setId(chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
 	}
 	
 }
