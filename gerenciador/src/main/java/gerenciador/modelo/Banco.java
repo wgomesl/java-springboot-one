f2b9a49cf01c2995e59e6a0cf7241de67256711b
wesley <wesley.gomes222@hotmail.com>
2023-04-21 11:46:00 -0300

    preparando modelo e view do form de login

--- gerenciador/src/main/java/gerenciador/modelo/Banco.java
+++ gerenciador/src/main/java/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -29,6 +30,17 @@ public class Banco {
 		lista.add(empresa2);
 		lista.add(empresa3);
 		lista.add(empresa4);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("wesley");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
