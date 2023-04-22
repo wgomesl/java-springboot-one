e82037f50aecc9519ad15e2e8e13a1db74496cb6
wesley <wesley.gomes222@hotmail.com>
2023-04-21 22:52:23 -0300

    finalizando client web service

--- gerenciador/src/main/java/gerenciador/servlet/EmpresasService.java
+++ gerenciador/src/main/java/gerenciador/servlet/EmpresasService.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import gerenciador.modelo.Banco;
@@ -18,31 +19,32 @@ import gerenciador.modelo.Empresa;
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		List<Empresa> empresas = new Banco().getEmpresas();
+
+		String valor = request.getHeader("Accept");
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);
-		
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
-		
-		
-		/*
-		 * Gson gson = new Gson(); String json = gson.toJson(empresas);
-		 * 
-		 * response.setContentType("application/json");
-		 * response.getWriter().print(json);
-		 */
+		System.out.println(valor);
+
+		if (valor.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+		} else if (valor.endsWith("json")) {
+			  Gson gson = new Gson(); String json = gson.toJson(empresas);
+			  
+			  response.setContentType("application/json");
+			  response.getWriter().print(json);
+		}else {
+			  response.setContentType("application/json");
+			  response.getWriter().print("{'message':'no content'}");
+		}
+		 
 	}
 
 }
-
-
-
-
-
-
-
