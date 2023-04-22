e3c644afb9b070d873ceb6b8802676a96faa0ad2
wesley <wesley.gomes222@hotmail.com>
2023-04-22 12:07:10 -0300

    alteração filter config

--- gerenciador/src/main/java/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/main/java/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,12 @@ import javax.servlet.http.HttpSession;
 //@WebFilter("/entrada")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig config) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
 		System.out.println("AutorizacaoFilter");
