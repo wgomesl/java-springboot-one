12b3f9a4f3c35cafe634190832a2f5981bf640c2
wesley <wesley.gomes222@hotmail.com>
2023-04-23 19:11:46 -0300

    ordenação

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,8 +32,8 @@ public class MedicoController {
 	}
 	
 	//listagem
-	@GetMapping
-	public Page<DadosListagemMedico> listar(Pageable paginacao){
+	@GetMapping                                           //carrega 10 registros e ordena pelo nome
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
 		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 }
