3a722d389755476d5210e8f0d8b0fa6898ed91d2
wesley <wesley.gomes222@hotmail.com>
2023-04-28 19:34:50 -0300

    testando o controle de acesso

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,19 +12,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 	
+	@Autowired
+	private SecurityFilter securityFilter;
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().authorizeRequests()
+				.and().authorizeHttpRequests()
 				.requestMatchers(HttpMethod.POST, "/login").permitAll()
 				.anyRequest().authenticated()
-				.and().build();
+				.and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
 	}
 	
 	@Bean
